47267e22ce874ac7682808dd514646c4a51f3715
brandiniA <90206743+brandiniA@users.noreply.github.com>
2022-06-10 13:33:50 -0500

    new build

--- src/views/LandingPage/index.js
+++ src/views/LandingPage/index.js
@@ -155,7 +155,7 @@ const LandingPage = () => {
                                     <div className="flex justify-center text-white text-lg md:text-2xl lg:text-4xl text-center">
                                         <div className="flex flex-col">
                                             <GenericCountDown
-                                                date={new Date(1654970400000)}
+                                                date={new Date(1654963200000)}
                                                 className="text-5xl md:text-8xl  xl:text-10xl"
                                             />
                                             <div
